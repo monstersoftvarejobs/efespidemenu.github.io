bcea0c7fed3ae50a8cf578e5cbc7398db68a6f59
monstersoftvare <127454284+monstersoftvarejobs@users.noreply.github.com>
2023-03-12 02:15:39 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,34 +1,35 @@
 
 
+
+
 const logoChangeWhite = document.getElementById("logo-change-white");
 const logoChangeDark =  document.getElementById("logo-change-dark");
 const navBarChange =  document.querySelector(".nav-bar");
 const aboutTextChange = document.getElementById("about-text-part");
 const menuGroupsChange = document.querySelector(".menu-groups");
+const changeButtonSpan = document.getElementById("background-change-span");
 
 console.log(navBarChange);
 
-//  logoChangeWhite.addEventListener("click",run);
-
-//  function run(){
-//    document.body.style.backgroundImage = "url('assets/hisar-burger-logo-1.jpg')";
-// };
 
 
 
 
-//  logoChangeDark.addEventListener("click",run2);
 
-//    function run2(){
-//      document.body.style.backgroundImage = "url('assets/hisar-burger-logo-2.jpg')";
-// };
+
+
+
+
 
 logoChangeWhite.addEventListener("click",run);
 function run(){
-    document.body.style.backgroundColor = "white";
-    navBarChange.style.color = "black";
+  document.body.style.backgroundImage = "url('assets/pide-salonu-background.png')";
+  navBarChange.style.color = "black";
     aboutTextChange.style.color =   "black";
-    // menuGroupsChange.style.backgroundColor = "#000000ce";
+    // changeButtonSpan.style.backgroundColor = "yellow";
+    logoChangeWhite.style.backgroundColor="";
+    logoChangeDark.style.backgroundColor="";
+
 
 
 
@@ -36,10 +37,15 @@ function run(){
 
 logoChangeDark.addEventListener("click",run2);
 function run2(){
-    document.body.style.backgroundColor = "black";
-    navBarChange.style.color = "white";
+  document.body.style.backgroundImage = "url('assets/pide-salonu-background-dark.png')";
+  navBarChange.style.color = "white";
     aboutTextChange.style.color =   "white";
-    // menuGroupsChange.style.backgroundColor="#cacacace";
+    // changeButtonSpan.style.backgroundColor = "red";
+    logoChangeDark.style.backgroundColor="";
+    logoChangeWhite.style.backgroundColor="";
+
+
+
 
 
 
@@ -76,10 +82,8 @@ function hideNonVisibleDivs() {
 
 
 
-// const map = document.getElementById("rez-maps-id");
 
 
-// console.log(map);
 
 
 
